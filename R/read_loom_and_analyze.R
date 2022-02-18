343cf7fde6ee8545b1ba281ba9885ca04bf34965
Ines Patop <inespatop@gmail.com>
2022-02-18 16:17:59 -0500

    Initial commit

--- R/read_loom_and_analyze.R
+++ R/read_loom_and_analyze.R
@@ -1,20 +1,16 @@
 #function to get "not in" from %in% in dplyr
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
-#read and analyze
 read_loom_and_analyze<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loom", plot=T) {
 
-  require(assertthat)
-
-  assert_that(
-    see_if(is.readable(loom_file))
+  assertthat::assert_that(
+    assertthat::see_if(assertthat::is.readable(loom_file))
   )
-
 
   print(paste0("running sample ",loom_file))
 
   #read loom
-  loom_object <- connect(filename = loom_file, mode = "r",skip.validate = T)
+  loom_object <- loomR::connect(filename = loom_file, mode = "r+",skip.validate = T)
 
   #extract genes
   gene.names <- loom_object[["row_attrs/Gene"]][]
@@ -26,15 +22,19 @@ read_loom_and_analyze<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loo
 
   # Pull three bits of metadata from the column attributes
   attrs <- c("n_counts", "n_genes", "age","ClusterID","CellID","tissue","sex","sample_id","percent_mito","fly_genetics","dissection_lab","batch_id","annotation")
-  attr.df <- loom_object$get.attribute.df(MARGIN = 2, attributes =  attrs)
+
+  #add cell_name atribute for it to run
+  loom_object$add.attribute(MARGIN = 2,list(cell_names = loom_object[["col_attrs/CellID"]][]))
+
+  #extract metadata
+  attr.df <- loom_object$get.attribute.df(MARGIN = 2, attribute.names  =  attrs)
 
   #close loom
   loom_object$close_all()
-  rm(loom_object)
 
   #read h5
-  Convert(gsub(loom_file, pattern = ".loom",replacement = ".h5ad"), dest = "h5seurat", overwrite = TRUE)
-  loom_object_h5<- LoadH5Seurat(gsub(loom_file, pattern = ".loom",replacement = ".h5seurat"))
+  SeuratDisk::Convert(gsub(loom_file, pattern = "x.loom",replacement = "x.h5ad"), dest = "h5seurat", overwrite = TRUE)
+  loom_object_h5<- SeuratDisk::LoadH5Seurat(gsub(loom_file, pattern = "x.loom",replacement = "x.h5seurat"))
 
   #attach names to reads matrix
   colnames(reads_matrix)<-colnames(loom_object_h5@assays$RNA@counts)
@@ -48,16 +48,16 @@ read_loom_and_analyze<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loo
 
   #create plots with basic analysis
   if (plot){
-    pdf(paste0("General_",nn,"_DimPlot_fromloom.pdf"))
-    p=DimPlot(loom_object_h5,repel = T,label = T,reduction = "umap",group.by = "annotation")+ NoLegend() + labs(title =  paste0("Clusters from loom ",nn))
+    grDevices::pdf(paste0("General_",nn,"_DimPlot_fromloom.pdf"))
+    p=Seurat::DimPlot(loom_object_h5,repel = T,label = T,reduction = "umap",group.by = "annotation")+ NoLegend() + labs(title =  paste0("Clusters from loom ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "tissue")+ labs(title = paste0("tissue ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "tissue")+ labs(title = paste0("tissue ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "sex")+ labs(title =  paste0("sex ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "sex")+ labs(title =  paste0("sex ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "age")+ labs(title =  paste0("age ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "age")+ labs(title =  paste0("age ",nn))
     print(p)
-    dev.off()
+    grDevices::dev.off()
   }
 
 
@@ -65,56 +65,57 @@ read_loom_and_analyze<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loo
 
   print("entering new seurat analysis")
   #create new seurat
-  new_seurat_obj <- CreateSeuratObject(counts = reads_matrix)
-  new_seurat_obj <- NormalizeData(object = new_seurat_obj)
-  new_seurat_obj <- FindVariableFeatures(object = new_seurat_obj)
-  new_seurat_obj <- ScaleData(object = new_seurat_obj)
-  new_seurat_obj <- RunPCA(object = new_seurat_obj)
-  new_seurat_obj <- FindNeighbors(object = new_seurat_obj,dims = 1:30)
-  new_seurat_obj <- FindClusters(object = new_seurat_obj)
-  new_seurat_obj <- RunTSNE(object = new_seurat_obj,dims = 1:30)
-  new_seurat_obj <- RunUMAP(object = new_seurat_obj,dims = 1:30)
+  new_seurat_obj <- SeuratObject::CreateSeuratObject(counts = reads_matrix)
+  new_seurat_obj <- Seurat::NormalizeData(object = new_seurat_obj)
+  new_seurat_obj <- Seurat::FindVariableFeatures(object = new_seurat_obj)
+  new_seurat_obj <- Seurat::ScaleData(object = new_seurat_obj)
+  new_seurat_obj <- Seurat::RunPCA(object = new_seurat_obj)
+  new_seurat_obj <- Seurat::FindNeighbors(object = new_seurat_obj,dims = 1:30)
+  new_seurat_obj <- Seurat::FindClusters(object = new_seurat_obj)
+  new_seurat_obj <- Seurat::RunTSNE(object = new_seurat_obj,dims = 1:30)
+  new_seurat_obj <- Seurat::RunUMAP(object = new_seurat_obj,dims = 1:30)
 
   #save old idents and transfere them
-  new_seurat_obj[["old.ident"]] <- Idents(object = new_seurat_obj)
+  new_seurat_obj[["old.ident"]] <- Seurat::Idents(object = new_seurat_obj)
   new_seurat_obj@meta.data=cbind(new_seurat_obj@meta.data,attr.df)
 
   #Now we can see our new analysis
   if (plot){
-    pdf(paste0(nn,"_DimPlot_newanalysis.pdf"))
+    grDevices::pdf(paste0(nn,"_DimPlot_newanalysis.pdf"))
 
-    p=DimPlot(new_seurat_obj,repel = T,label = T,reduction = "tsne")+ labs(title = nn)
+    p=Seurat::DimPlot(new_seurat_obj,repel = T,label = T,reduction = "tsne")+ labs(title = nn)
     print(p)
-    p=DimPlot(new_seurat_obj,repel = T,label = T,reduction = "tsne", group.by = "annotation")+ NoLegend() + labs(title = nn)
+    p=Seurat::DimPlot(new_seurat_obj,repel = T,label = T,reduction = "tsne", group.by = "annotation")+ NoLegend() + labs(title = nn)
     print(p)
-    p=DimPlot(new_seurat_obj,repel = T,label = T,reduction = "umap")+ labs(title = nn)
+    p=Seurat::DimPlot(new_seurat_obj,repel = T,label = T,reduction = "umap")+ labs(title = nn)
     print(p)
-    p=DimPlot(new_seurat_obj,repel = T,label = T,reduction = "umap", group.by = "annotation")+ NoLegend() + labs(title = nn)
+    p=Seurat::DimPlot(new_seurat_obj,repel = T,label = T,reduction = "umap", group.by = "annotation")+ NoLegend() + labs(title = nn)
 
     print(p)
 
-    dev.off()
+    grDevices::dev.off()
   }
 
   print("calculting markers")
   #get markers for new ID
   new_seurat_obj@active.ident<-as.factor(new_seurat_obj$old.ident)
-  new_seurat_obj<-RenameCells(new_seurat_obj,new.names =new_seurat_obj$CellID)
-  all.markers.new<-FindAllMarkers(new_seurat_obj,only.pos = T)
+  new_seurat_obj<-Seurat::RenameCells(new_seurat_obj,new.names =new_seurat_obj$CellID)
+  all.markers.new<-Seurat::FindAllMarkers(new_seurat_obj,only.pos = T)
   #get markers for paper ID
   new_seurat_obj@active.ident<-as.factor(gsub(pattern = " ",replacement = "",new_seurat_obj$annotation))
-  all.markers.paper<-FindAllMarkers(new_seurat_obj,only.pos = T)
+  all.markers.paper<-Seurat::FindAllMarkers(new_seurat_obj,only.pos = T)
   #extract top10
-  top10markers <- all.markers.paper %>% group_by(cluster) %>%top_n(n = 10, wt = avg_log2FC)
+  top10markers <- all.markers.paper %>% dplyr::group_by(cluster) %>% dplyr::top_n(n = 10, wt = avg_log2FC)
+
 
   if(plot){
-    pdf(paste0(nn,"_markers_plots",".pdf"),width = 15,height = 30)
+    grDevices::pdf(paste0(nn,"_markers_plots",".pdf"),width = 15,height = 30)
 
-    cluster.averages <- AverageExpression(new_seurat_obj, return.seurat = TRUE, add.ident = "sex")
-    p=DoHeatmap(cluster.averages, features = top10markers$gene) + NoLegend()
+    cluster.averages <- Seurat::AverageExpression(new_seurat_obj, return.seurat = TRUE, add.ident = "sex")
+    p=Seurat::DoHeatmap(cluster.averages, features = top10markers$gene) + NoLegend()
     print(p)
 
-    dev.off()
+    grDevices::dev.off()
   }
 
   #now we rename the objects and save the data
@@ -132,19 +133,18 @@ read_loom_and_analyze<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loo
 
 }
 
+
 loom_too_Seurat<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loom", plot=T) {
 
-  require(assertthat)
 
-  assert_that(
-    see_if(is.readable(loom_file))
+  assertthat::assert_that(
+    assertthat::see_if(assertthat::is.readable(loom_file))
   )
-
 
   print(paste0("running sample ",loom_file))
 
   #read loom
-  loom_object <- connect(filename = loom_file, mode = "r",skip.validate = T)
+  loom_object <- loomR::connect(filename = loom_file, mode = "r+",skip.validate = T)
 
   #extract genes
   gene.names <- loom_object[["row_attrs/Gene"]][]
@@ -156,15 +156,19 @@ loom_too_Seurat<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loom", pl
 
   # Pull three bits of metadata from the column attributes
   attrs <- c("n_counts", "n_genes", "age","ClusterID","CellID","tissue","sex","sample_id","percent_mito","fly_genetics","dissection_lab","batch_id","annotation")
-  attr.df <- loom_object$get.attribute.df(MARGIN = 2, attributes =  attrs)
+
+  #add cell_name atribute for it to run
+  loom_object$add.attribute(MARGIN = 2,list(cell_names = loom_object[["col_attrs/CellID"]][]))
+
+  #extract metadata
+  attr.df <- loom_object$get.attribute.df(MARGIN = 2, attribute.names  =  attrs)
 
   #close loom
   loom_object$close_all()
-  rm(loom_object)
 
   #read h5
-  Convert(gsub(loom_file, pattern = ".loom",replacement = ".h5ad"), dest = "h5seurat", overwrite = TRUE)
-  loom_object_h5<- LoadH5Seurat(gsub(loom_file, pattern = ".loom",replacement = ".h5seurat"))
+  SeuratDisk::Convert(gsub(loom_file, pattern = "x.loom",replacement = "x.h5ad"), dest = "h5seurat", overwrite = TRUE)
+  loom_object_h5<- SeuratDisk::LoadH5Seurat(gsub(loom_file, pattern = "x.loom",replacement = "x.h5seurat"))
 
   #attach names to reads matrix
   colnames(reads_matrix)<-colnames(loom_object_h5@assays$RNA@counts)
@@ -176,36 +180,32 @@ loom_too_Seurat<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loom", pl
   nn=gsub(pattern = "s_fca_biohub_",replacement = "",basename(loom_file))
   nn=gsub(pattern = "_10x.loom",replacement = "",nn)
 
-  return(loom_object_h5)
-
   #create plots with basic analysis
   if (plot){
-    pdf(paste0("General_",nn,"_DimPlot_fromloom.pdf"))
-    p=DimPlot(loom_object_h5,repel = T,label = T,reduction = "umap",group.by = "annotation")+ NoLegend() + labs(title =  paste0("Clusters from loom ",nn))
+    grDevices::pdf(paste0("General_",nn,"_DimPlot_fromloom.pdf"))
+    p=Seurat::DimPlot(loom_object_h5,repel = T,label = T,reduction = "umap",group.by = "annotation")+ NoLegend() + labs(title =  paste0("Clusters from loom ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "tissue")+ labs(title = paste0("tissue ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "tissue")+ labs(title = paste0("tissue ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "sex")+ labs(title =  paste0("sex ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "sex")+ labs(title =  paste0("sex ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "age")+ labs(title =  paste0("age ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "age")+ labs(title =  paste0("age ",nn))
     print(p)
-    dev.off()
+    grDevices::dev.off()
   }
+
+  return(loom_object_h5)
 }
 
 loom_too_Seurat_brandNew<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.loom", plot=T,preprocesing=T) {
-
-  require(assertthat)
-
-  assert_that(
-    see_if(is.readable(loom_file))
+  assertthat::assert_that(
+    assertthat::see_if(assertthat::is.readable(loom_file))
   )
-
 
   print(paste0("running sample ",loom_file))
 
   #read loom
-  loom_object <- connect(filename = loom_file, mode = "r",skip.validate = T)
+  loom_object <- loomR::connect(filename = loom_file, mode = "r+",skip.validate = T)
 
   #extract genes
   gene.names <- loom_object[["row_attrs/Gene"]][]
@@ -217,15 +217,19 @@ loom_too_Seurat_brandNew<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.
 
   # Pull three bits of metadata from the column attributes
   attrs <- c("n_counts", "n_genes", "age","ClusterID","CellID","tissue","sex","sample_id","percent_mito","fly_genetics","dissection_lab","batch_id","annotation")
-  attr.df <- loom_object$get.attribute.df(MARGIN = 2, attributes =  attrs)
+
+  #add cell_name atribute for it to run
+  loom_object$add.attribute(MARGIN = 2,list(cell_names = loom_object[["col_attrs/CellID"]][]))
+
+  #extract metadata
+  attr.df <- loom_object$get.attribute.df(MARGIN = 2, attribute.names  =  attrs)
 
   #close loom
   loom_object$close_all()
-  rm(loom_object)
 
   #read h5
-  Convert(gsub(loom_file, pattern = ".loom",replacement = ".h5ad"), dest = "h5seurat", overwrite = TRUE)
-  loom_object_h5<- LoadH5Seurat(gsub(loom_file, pattern = ".loom",replacement = ".h5seurat"))
+  SeuratDisk::Convert(gsub(loom_file, pattern = "x.loom",replacement = "x.h5ad"), dest = "h5seurat", overwrite = TRUE)
+  loom_object_h5<- SeuratDisk::LoadH5Seurat(gsub(loom_file, pattern = "x.loom",replacement = "x.h5seurat"))
 
   #attach names to reads matrix
   colnames(reads_matrix)<-colnames(loom_object_h5@assays$RNA@counts)
@@ -237,64 +241,59 @@ loom_too_Seurat_brandNew<-function(loom_file="./loom/s_fca_biohub_body_wall_10x.
   nn=gsub(pattern = "s_fca_biohub_",replacement = "",basename(loom_file))
   nn=gsub(pattern = "_10x.loom",replacement = "",nn)
 
-  return(loom_object_h5)
-
   #create plots with basic analysis
   if (plot){
-    pdf(paste0("General_",nn,"_DimPlot_fromloom.pdf"))
-    p=DimPlot(loom_object_h5,repel = T,label = T,reduction = "umap",group.by = "annotation")+ NoLegend() + labs(title =  paste0("Clusters from loom ",nn))
+    grDevices::pdf(paste0("General_",nn,"_DimPlot_fromloom.pdf"))
+    p=Seurat::DimPlot(loom_object_h5,repel = T,label = T,reduction = "umap",group.by = "annotation")+ NoLegend() + labs(title =  paste0("Clusters from loom ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "tissue")+ labs(title = paste0("tissue ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "tissue")+ labs(title = paste0("tissue ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "sex")+ labs(title =  paste0("sex ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "sex")+ labs(title =  paste0("sex ",nn))
     print(p)
-    p=DimPlot(loom_object_h5,reduction = "umap",group.by = "age")+ labs(title =  paste0("age ",nn))
+    p=Seurat::DimPlot(loom_object_h5,reduction = "umap",group.by = "age")+ labs(title =  paste0("age ",nn))
     print(p)
-    dev.off()
+    grDevices::dev.off()
   }
+
 
   #To be able to see any gene you are interested in you have to re-do the basic analysis from screatch. This is because the provided file only has most variable genes
 
   print("entering new seurat analysis")
   #create new seurat
-  new_seurat_obj <- CreateSeuratObject(counts = reads_matrix)
+  new_seurat_obj <- SeuratObject::CreateSeuratObject(counts = reads_matrix)
 
   if(preprocesing){
-    new_seurat_obj <- NormalizeData(object = new_seurat_obj)
-    new_seurat_obj <- FindVariableFeatures(object = new_seurat_obj)
-    new_seurat_obj <- ScaleData(object = new_seurat_obj)
-    new_seurat_obj <- RunPCA(object = new_seurat_obj)
-    new_seurat_obj <- FindNeighbors(object = new_seurat_obj,dims = 1:30)
-    new_seurat_obj <- FindClusters(object = new_seurat_obj)
-    new_seurat_obj <- RunTSNE(object = new_seurat_obj,dims = 1:30)
-    new_seurat_obj <- RunUMAP(object = new_seurat_obj,dims = 1:30)
+    new_seurat_obj <- Seurat::NormalizeData(object = new_seurat_obj)
+    new_seurat_obj <- Seurat::FindVariableFeatures(object = new_seurat_obj)
+    new_seurat_obj <- Seurat::ScaleData(object = new_seurat_obj)
+    new_seurat_obj <- Seurat::RunPCA(object = new_seurat_obj)
+    new_seurat_obj <- Seurat::FindNeighbors(object = new_seurat_obj,dims = 1:30)
+    new_seurat_obj <- Seurat::FindClusters(object = new_seurat_obj)
+    new_seurat_obj <- Seurat::RunTSNE(object = new_seurat_obj,dims = 1:30)
+    new_seurat_obj <- Seurat::RunUMAP(object = new_seurat_obj,dims = 1:30)
 
     #save old idents and transfere them
-    new_seurat_obj[["old.ident"]] <- Idents(object = new_seurat_obj)
+    new_seurat_obj[["old.ident"]] <- Seurat::Idents(object = new_seurat_obj)
     new_seurat_obj@meta.data=cbind(new_seurat_obj@meta.data,attr.df)
 
     #Now we can see our new analysis
     if (plot){
-      pdf(paste0(nn,"_DimPlot_newanalysis.pdf"))
+      grDevices::pdf(paste0(nn,"_DimPlot_newanalysis.pdf"))
 
-      p=DimPlot(new_seurat_obj,repel = T,label = T,reduction = "tsne")+ labs(title = nn)
+      p=Seurat::DimPlot(new_seurat_obj,repel = T,label = T,reduction = "tsne")+ labs(title = nn)
       print(p)
-      p=DimPlot(new_seurat_obj,repel = T,label = T,reduction = "tsne", group.by = "annotation")+ NoLegend() + labs(title = nn)
+      p=Seurat::DimPlot(new_seurat_obj,repel = T,label = T,reduction = "tsne", group.by = "annotation")+ NoLegend() + labs(title = nn)
       print(p)
-      p=DimPlot(new_seurat_obj,repel = T,label = T,reduction = "umap")+ labs(title = nn)
+      p=Seurat::DimPlot(new_seurat_obj,repel = T,label = T,reduction = "umap")+ labs(title = nn)
       print(p)
-      p=DimPlot(new_seurat_obj,repel = T,label = T,reduction = "umap", group.by = "annotation")+ NoLegend() + labs(title = nn)
+      p=Seurat::DimPlot(new_seurat_obj,repel = T,label = T,reduction = "umap", group.by = "annotation")+ NoLegend() + labs(title = nn)
 
       print(p)
 
-      dev.off()
+      grDevices::dev.off()
     }
-
   }
-
   return(new_seurat_obj)
 
-
 }
-
 
