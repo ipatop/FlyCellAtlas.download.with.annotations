b96a2beb118760d86fa8fead8cb30870adba3dc6
ipatop <52970373+ipatop@users.noreply.github.com>
2022-02-21 12:29:11 -0500

    vignette
    add vignettes

---
+++ doc/read_loom_and_analyze.R
@@ -0,0 +1,10 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+#devtools::install_github("ipatop/FlyCellAtlas.download.with.annotations")
+library(FlyCellAtlas.download.with.annotations)
+
